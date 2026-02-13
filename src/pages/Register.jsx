28e80b5a29954cf8c694ddba3dc0298d54449bcb
Adam Faiq <adamfaiq62@gmail.com>
2026-02-14 03:37:52 +0800

    add setError useState

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const register = async () => {
